f0d7a5fce08c23508286ec5471cf5fb461514098
Abdullah Ilhanli <abdullah.ilhanli@amadeus.com>
2018-10-12 15:46:24 +0300

    Revert "silly delete source codes"
    This reverts commit ca5f159b8f1b9c4805e05c02796c0b65d20581ac.

---
+++ GuessCapital.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class GuessCapital{
+	public static void main(String[]args){
+		System.out.println("Welcome to Guess Game!");
+		
+		try(Scanner scanner = new Scanner(System.in)){
+			System.out.println("Please enter capital of Turkey");
+			String lineEntered= scanner.next();
+			if(lineEntered.equals("Ankara")){
+				System.out.println("correct!");
+			}else{
+				System.out.println("wrong!");
+			}
+		}catch(Exception e){
+		}
+	}
+}
